29ad16b789e8ffa62203d6e87bd6f4c0aefc491a
KaleabKindu <kkmichaelstarkk@gmail.com>
2024-04-08 12:05:51 +0300

    implement header ui

---
+++ src/components/common/Header.jsx
@@ -0,0 +1,61 @@
+import Image from "next/image";
+import Link from "next/link";
+import { IoSearchOutline } from "react-icons/io5";
+import { VscAccount } from "react-icons/vsc";
+const nav_links = [
+  {
+    name: "Browse Categories",
+    submenu: true,
+    route: "",
+  },
+  {
+    name: "Blog",
+    submenu: false,
+    route: "",
+  },
+  {
+    name: "Write A Review",
+    submenu: false,
+    route: "",
+  },
+];
+const Header = (props) => {
+  return (
+    <div className="flex justify-between p-3">
+      {/* first section */}
+      <div className="flex items-center gap-5">
+        <Image src="/logo.svg" width={50} height={50} alt="helperzz logo" />
+        <div className="gap-5 hidden lg:flex">
+          {nav_links.map((link, index) => (
+            <Link href={link.route} key={index}>
+              {link.name}
+            </Link>
+          ))}
+        </div>
+      </div>
+
+      {/* second section */}
+      <div className="border-[#D9D9D9] divide-x-2 divide-[#D9D9D9] hidden lg:flex border rounded-2xl pl-3">
+        <input
+          className="rounded-lg focus:outline-none mr-2"
+          placeholder="Search for Category or Company"
+          type="text"
+        />
+        <div className="flex justify-center items-center w-[100px] px-2">
+          Oshawa
+        </div>
+        <div className="flex justify-center items-center px-2">
+          <IoSearchOutline className="text-[#000000] opacity-50" size={30} />
+        </div>
+      </div>
+
+      {/* third section */}
+      <div className="flex gap-3 items-center">
+        <div className="border-2 border-black w-12 h-12 rounded-full" />
+        <p>Jane</p>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
